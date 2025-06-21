ff82b6a4604f072c2c84f610ac68294d61107594
artizanw <artizanw1@gmail.com>
2025-06-21 00:34:07 -0400

    timer

---
+++ app/src/components/timer/Timer.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import './Timer.css';
+
+const Timer = () => {
+	return (
+		<div className="Timer">
+			<h1> MEOW </h1>
+		</div>
+	)
+}
+
+export default Timer;
